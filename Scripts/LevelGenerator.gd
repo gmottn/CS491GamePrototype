2ba846957b98e77798161bd75b21860ae31a07cb
AhmedAbdulFaisal <ahmedabdulfaisal@github.com>
2023-11-18 16:57:00 -0500

    added alot of generation stuff

--- Scripts/LevelGenerator.gd
+++ Scripts/LevelGenerator.gd
@@ -2,8 +2,9 @@ extends Spatial
 
 const dir = [Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN]
 
-var grid_size = 200
-var grid_steps = 2000
+var grid_size = 10
+var grid_steps = 100
+
 
 
 
@@ -14,6 +15,12 @@ func _ready():
 	var current_dir = Vector2.RIGHT
 	
 	var last_dir = current_dir * -1
+		
+	for x in range(-12,12):
+		for y in range(-12,12):
+			$GridMap.set_cell_item(x, -1, y, 0)
+			$GridMap.set_cell_item(x, 0, y, 0)
+			$GridMap.set_cell_item(x, 1, y, 0)
 	
 	for i in range(0,grid_steps):
 		var temp_dir = dir.duplicate()
@@ -21,13 +28,21 @@ func _ready():
 		var d = temp_dir.pop_front()
 		
 		while (abs(current_pos.x + d.x) > grid_size or 
-		abs(current_pos.y + d.y) > grid_size or
-		d == last_dir * -1):
+		abs(current_pos.y + d.y) > grid_size or d == last_dir * -1):
 			temp_dir.shuffle()
 			d = temp_dir.pop_front()
 			
 		current_pos += d
 		last_dir = d
 		
-		$GridMap.set_cell_item(current_pos.x, 0, current_pos.y, 0)
+		
+		
+		$GridMap.set_cell_item(current_pos.x, -1, current_pos.y, 2)
+		$GridMap.set_cell_item(current_pos.x, 0, current_pos.y, -1)
+		$GridMap.set_cell_item(current_pos.x, 1, current_pos.y, 2)
+		
+	
+	# set up player placement here
+	
+	
 
